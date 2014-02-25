6b5c1c698bbb528be4124b1231433ef6d510cfea
dmitru <dmitriy.borodiy@gmail.com>
2014-02-25 20:37:38 +0400

    JSF demonstration added

--- ScreencastExampleApp/ScreencastExampleApp/src/java/ru/dmitryborody/webexampleapp/model/LoginBean.java
+++ ScreencastExampleApp/ScreencastExampleApp/src/java/ru/dmitryborody/webexampleapp/model/LoginBean.java
@@ -6,6 +6,8 @@
 
 package ru.dmitryborody.webexampleapp.model;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import ru.dmitryborody.webexampleapp.model.Registry;
 import ru.dmitryborody.webexampleapp.model.User;
 
@@ -13,9 +15,20 @@ import ru.dmitryborody.webexampleapp.model.User;
  *
  * @author Dmitry
  */
+@ManagedBean
+@SessionScoped
 public class LoginBean {
     String name;
     String password;
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
     User me;
     
     public LoginBean() {
@@ -31,6 +44,22 @@ public class LoginBean {
             } 
         }
         return false;
+    }
+    
+    public String loginJsf() {
+        for (User u : Registry.getInstance().getUsers()) {
+            if (u.getName().equals(name)) {
+                if (u.getPassword().equals(password)) {
+                    me = u;
+                    return "profile.xhtml";
+                }
+            } 
+        }
+        return "login.xhtml";
+    }
+    
+    public void sendMessage(User to) {
+        me.sendMessage(message, to);
     }
 
     public String getName() {
